e35573e8501affea361048becd301d1043cbaa5b
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2024-07-29 20:16:45 -0300

    Adicionando_Players

--- Pong_Main.py
+++ Pong_Main.py
@@ -5,8 +5,17 @@ pygame.init()
 window = pygame.display.set_mode([1280, 720])
 title = pygame.display.set_caption("Pong")
 
-bg = pygame.image.load("assets/field.png")
-window.blit(bg, (0, 0))
+field = pygame.image.load("assets/field.png")
+window.blit(field, (0, 0))
+
+player1 = pygame.image.load("assets/player1.png")
+window.blit(player1, (50, 310))
+
+player2 = pygame.image.load("assets/player2.png")
+window.blit(player2, (1150, 310))
+
+ball = pygame.image.load("assets/ball.png")
+window.blit(ball, (617, 337))
 
 loop = True
 while loop:
