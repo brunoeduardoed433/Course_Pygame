4c6cb9fdd8744aa11ae74058a2b48380c3d09936
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2024-07-30 20:10:57 -0300

    Movimentação_Bolinha

--- Pong_Main.py
+++ Pong_Main.py
@@ -1,21 +1,37 @@
 import pygame
 
+
 pygame.init()
+
 
 window = pygame.display.set_mode([1280, 720])
 title = pygame.display.set_caption("Pong")
 
+
 field = pygame.image.load("assets/field.png")
-window.blit(field, (0, 0))
-
 player1 = pygame.image.load("assets/player1.png")
-window.blit(player1, (50, 310))
-
 player2 = pygame.image.load("assets/player2.png")
-window.blit(player2, (1150, 310))
-
 ball = pygame.image.load("assets/ball.png")
-window.blit(ball, (617, 337))
+
+
+ball_x = 617
+ball_y = 337
+
+
+def move_ball():
+
+    global ball_x
+    global ball_y
+
+    ball_x += 1
+
+
+def draw():
+    window.blit(field, (0, 0))
+    window.blit(player1, (50, 310))
+    window.blit(player2, (1150, 310))
+    window.blit(ball, (ball_x, ball_y))
+
 
 loop = True
 while loop:
@@ -23,5 +39,8 @@ while loop:
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             loop = False
+
+    draw()
+    move_ball()
 
     pygame.display.update()       
