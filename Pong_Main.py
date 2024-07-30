59807e88e7b3835550d42d9793f652adf9505bae
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2024-07-30 20:27:08 -0300

    Movimentando_Player

--- Pong_Main.py
+++ Pong_Main.py
@@ -9,11 +9,14 @@ title = pygame.display.set_caption("Pong")
 
 
 field = pygame.image.load("assets/field.png")
+
 player1 = pygame.image.load("assets/player1.png")
+player1_y = 310
+
 player2 = pygame.image.load("assets/player2.png")
+player2_y = 310
+
 ball = pygame.image.load("assets/ball.png")
-
-
 ball_x = 617
 ball_y = 337
 
@@ -28,8 +31,8 @@ def move_ball():
 
 def draw():
     window.blit(field, (0, 0))
-    window.blit(player1, (50, 310))
-    window.blit(player2, (1150, 310))
+    window.blit(player1, (50, player1_y))
+    window.blit(player2, (1150, player2_y))
     window.blit(ball, (ball_x, ball_y))
 
 
@@ -39,6 +42,18 @@ while loop:
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             loop = False
+        if events.type == pygame.KEYDOWN:
+            if events.key == pygame.K_w:
+                player1_y -= 10
+            else:
+                if events.key == pygame.K_s:
+                    player1_y += 10
+                else:
+                    if events.key ==  pygame.K_UP:
+                        player2_y -= 10
+                    else:
+                        if events.key == pygame.K_DOWN:
+                            player2_y += 10
 
     draw()
     move_ball()
